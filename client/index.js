a7d707f40a477a072d31e04d0023c9b96bc182d5
Adam Cox <adamcox.dev@gmail.com>
2022-10-04 19:24:22 -0600

    Added post request

--- client/index.js
+++ client/index.js
@@ -12,21 +12,36 @@ for(let i =0; i < arr.length; i++){
   }
 }
 
-const personalTattooList = (name) => {
-    const newList = document.createElement('section')
-    newList.classList.add()
+// const personalTattooList = (name) => {
+//     const newList = document.createElement('section')
+//     newList.classList.add()
 
-    newList.innerHTML = `
-    
-    `
-}
+//     newList.innerHTML = `
+//     <form>
+//     <input 
+//         type='text'
+//         placeholder='Your name'/>
+//     <input 
+//         type='text'
+//         placeholder='The name of the new tattoo!'/>
+//     <input 
+//         type='text'
+//         id="img"
+//         placeholder='Paste an image URL'/>
+//     <button>add house</button>
+// </form>
+
+//     `
+// }
+
+
 
 const createTattooList = (tattoo) => {
     const tattooList = document.createElement('section')
     tattooList.classList.add('tattoo-list')
 
     tattooList.innerHTML = `
-    <img src = ${tattoo.picture} alt='Tattoo Image' onclick="getList(${{id: tattoo.id, name: tattoo.name, picture: tattoo.picture}})" />
+    <img id = "ink1" src = ${tattoo.picture} alt='Tattoo Image' onclick="getList(${{id: tattoo.id, name: tattoo.name, picture: tattoo.picture}})" />
     <p>${tattoo.name}</p>
 
     `
@@ -44,11 +59,20 @@ const getAllTattoos = () => {
     })
 }
 
-const getList = (bodyObj) => {
+const addTattoo = () => {
+   let newInput = document.querySelector('#new-tattoo')
+   let newImage = document.querySelector('#img-new')
+    let bodyObj = { 
+        name: newInput.value,
+        picture: newImage.value
+    }
     axios.post(`${baseURL}/addToList`, bodyObj)
     .then((res) => {
-        makeList(res.data)
+        showTattoos.innerHTML = ''
+        displayTattoos(res.data)
         console.log(res.data)
+        newInput.value = ''
+        newImage.value = ''
     })
     .catch((err) => {
         console.log(err)
@@ -58,3 +82,4 @@ const getList = (bodyObj) => {
 
 
 document.querySelector('#dropDown').addEventListener('click', getAllTattoos);
+document.querySelector('#new-btn').addEventListener('click', addTattoo)
--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,5 @@
 const tattoos = require('./db.json');
+let globalID = 9;
 
 const myTattoos = [];
 
@@ -13,9 +14,17 @@ module.exports = {
     res.status(200).send(tattoos)
     },
     addToList: (req, res) => {
-        // const {name} = req.body;
-        myTattoos.push(req.body)
-        res.status(200).send(myTattoos)
+        const {name, picture} = req.body;
+        let newTatt = {
+            id: globalID,
+            name,
+            picture
+        }
+        console.log(newTatt)
+        tattoos.push(newTatt);
+        globalID++;
+        res.status(200).send(tattoos);
+        
     }
     // deleteTattoo: (req, res) => {
     //     const index = tattoos.findIndex(el => el.id === +req.params.id)
